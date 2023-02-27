09e6765290136516b67b39377b627dc54eb4750c
Kylie West <kmichellewest@gmail.com>
2023-02-27 12:38:29 -0800

    Create TweetResponseDto

---
+++ twitter_api/src/main/java/com/cooksys/twitter_api/dtos/TweetResponseDto.java
@@ -0,0 +1,24 @@
+package com.cooksys.twitter_api.dtos;
+
+import java.sql.Timestamp;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class TweetResponseDto {
+	
+	private Integer id;
+	
+	private UserResponseDto author;
+	
+	private Timestamp posted;
+	
+	private String content;
+	
+	private TweetResponseDto inReplyTo;
+	
+	private TweetResponseDto repostOf;
+	
+}
