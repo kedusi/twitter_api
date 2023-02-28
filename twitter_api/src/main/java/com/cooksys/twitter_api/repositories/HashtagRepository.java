159cb93bedb9b352d7f4741d0d0327c4810838a5
kedusi <kedusi@gmail.com>
2023-02-28 01:39:49 -0600

    HashtagRepository as interface, @Repository

--- twitter_api/src/main/java/com/cooksys/twitter_api/repositories/HashtagRepository.java
+++ twitter_api/src/main/java/com/cooksys/twitter_api/repositories/HashtagRepository.java
@@ -1,5 +1,11 @@
 package com.cooksys.twitter_api.repositories;
 
-public class HashtagRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cooksys.twitter_api.entities.Hashtag;
+
+@Repository
+public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
 
 }
