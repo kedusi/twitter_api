464c62fc1820bcb349e37b7c685292700cfd5563
kedusi <kedusi@gmail.com>
2023-02-27 21:31:09 -0600

    create htController; create htService; returns [] for GetAllHashtags

---
+++ twitter_api/src/main/java/com/cooksys/twitter_api/controllers/HashtagController.java
@@ -0,0 +1,27 @@
+package com.cooksys.twitter_api.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cooksys.twitter_api.entities.Hashtag;
+import com.cooksys.twitter_api.services.HashtagService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("tags")
+public class HashtagController {
+
+	private HashtagService hashtagService;
+	
+	@GetMapping
+	private List<Hashtag> GetAllHashtags(){
+		return new ArrayList<>();
+	}
+	
+}
