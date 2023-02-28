8ca3531ee13c7e93d4dc077a9b91f60654c877de
kedusi <kedusi@gmail.com>
2023-02-28 00:54:05 -0600

    hashtagEntityToDto Mapper

--- twitter_api/src/main/java/com/cooksys/twitter_api/mappers/HashtagMapper.java
+++ twitter_api/src/main/java/com/cooksys/twitter_api/mappers/HashtagMapper.java
@@ -1,6 +1,12 @@
 package com.cooksys.twitter_api.mappers;
 
+import org.mapstruct.Mapper;
 
+import com.cooksys.twitter_api.dtos.HashtagResponseDto;
+import com.cooksys.twitter_api.entities.Hashtag;
+
+@Mapper(componentModel = "spring", uses = {HashtagMapper.class})
 public interface HashtagMapper {
 
+	HashtagResponseDto hashtagEntityToDto(Hashtag hashtag);
 }
