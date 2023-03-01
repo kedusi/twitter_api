269d289ea4385e1848e3e3c49ace6743045db57b
Helena Makendengue <HelenaMakens1013@gmail.com>
2023-03-01 12:55:56 -0600

    Update ProfileMapper.java

--- twitter_api/src/main/java/com/cooksys/twitter_api/mappers/ProfileMapper.java
+++ twitter_api/src/main/java/com/cooksys/twitter_api/mappers/ProfileMapper.java
@@ -5,7 +5,7 @@ import org.mapstruct.Mapper;
 import com.cooksys.twitter_api.dtos.ProfileRequestDto;
 import com.cooksys.twitter_api.entities.Profile;
 
-@Mapper(componentModel = "spring", uses = { UserMapper.class })
+@Mapper(componentModel = "spring")
 public interface ProfileMapper {
 	
 	Profile requestDtoToEntity(ProfileRequestDto profileRequestDto);
