85fe73f2fd2480dbd8d4d2d02feb96514782829e
Kylie West <kmichellewest@gmail.com>
2023-03-01 16:12:29 -0800

    Implement getContext method in TweetServiceImpl

--- twitter_api/src/main/java/com/cooksys/twitter_api/repositories/TweetRepository.java
+++ twitter_api/src/main/java/com/cooksys/twitter_api/repositories/TweetRepository.java
@@ -15,4 +15,7 @@ public interface TweetRepository extends JpaRepository<Tweet, Long> {
 	
 	List<Tweet> findAllByDeletedFalse();
 	
+	// Get all tweets replying to tweet with given id
+	List<Tweet> findAllByReplyTo_id(Long id);
+	
 }
